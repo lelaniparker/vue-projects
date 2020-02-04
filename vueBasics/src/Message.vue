880dc9eec456a01d1922507f447948010a002e1f
lelani82 <lelaniparker@hotmail.com>
2020-02-04 12:19:36 +1000

    started vue basics app

---
+++ vueBasics/src/Message.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Learning All About Vue</h1>
+    <app-input></app-input>
+  </div>
+</template>
+
+<script>
+import Input from "./Input"; // Need to import file to use their components
+
+export default {
+  //Use components here when only using it in 1 file (else import/export into Main file)
+  components: {
+    "app-input": Input // selector: object imported (then add selector to template above)
+  }
+};
+</script>
